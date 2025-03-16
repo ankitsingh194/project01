936de12c04275f6dcc5c65505e5c499f73177bbd
ankitsingh194 <53599855+ankitsingh194@users.noreply.github.com>
2025-03-16 15:49:40 +0530

    db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://ankit1602121007:FDfUTe4a42j6Gm6p@cluster0.f71lwn2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "";
 
 
 
